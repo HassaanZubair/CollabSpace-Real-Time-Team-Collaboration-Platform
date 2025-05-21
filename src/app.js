2f0027fd66bae8909677df64a34c644b24c1d859
Hassaan Zubair <hassaanzubair2@gmail.com>
2025-05-21 17:47:34 +0500

    Database added and table created with ORM

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const sequelize = require('./config/db');
+const User = require('./models/User');
 const express = require('express');
 const app = express();
 
@@ -6,5 +8,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello from first Collabspace App');
 });
-
+sequelize.sync()
+.then(() => { console.log('Posgresql Database sync'); })
+.catch((err) => { console.log('Error syncing with database, database dont connected', err); });
 module.exports = app;
